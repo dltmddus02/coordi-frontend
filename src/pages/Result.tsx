4e9516dbefff623f68256ecaf6f1d05898ebf6d6
dltmddus <tmddus4671@gmail.com>
2024-02-15 21:55:43 +0900

    add: uploadTop3Image 함수 추가

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -21,14 +21,15 @@ export default function Result() {
   //       console.error('Error fetching data:', error)
   //     })
   // }, [])
+
   /* 백엔드에서 데이터 가져오기 */
   useEffect(() => {
-    fetchData()
+    fetchData('http://127.0.0.1:8000/api/upload/')
   }, [])
 
-  const fetchData = () => {
+  const fetchData = (url: string) => {
     axios
-      .get('http://127.0.0.1:8000/api/upload/')
+      .get(url)
       .then(response => {
         setResultData(response.data)
       })
@@ -38,14 +39,31 @@ export default function Result() {
   }
 
   const refreshRecommendation = () => {
-    fetchData()
+    fetchData('http://127.0.0.1:8000/api/result/')
+  }
+
+  const uploadTop3Image = () => {
+    axios
+      .post('http://127.0.0.1:8000/api/result/')
+      .then(response => {
+        console.log(response.data.topk)
+        // 데이터 화면에 표시
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
   }
 
   return (
     <main className="mt-4 ml-4">
       <Title>추천 코디</Title>
-      <button onClick={refreshRecommendation} className="mb-4">
-        코디 추천하기
+      <button
+        type="submit"
+        onClick={refreshRecommendation}
+        onChange={uploadTop3Image}
+        className="w-full mt-4 ml-4 bg-gray-300 border border-gray-500 rounded-md"
+        style={{width: '13rem', height: '2rem'}}>
+        <p className="text-sm">결과를 보려면 클릭해주세요!</p>
       </button>
       <div className="flex flex-row">
         {/* 상의 3벌 데이터 가져오기 */}
